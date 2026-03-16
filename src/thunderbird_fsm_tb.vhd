11f73e102b3dda50eabe8a9b8a6054f609738257
lillianbuchanan2006 <lillianbuchanan2006@users.noreply.github.com>
2026-03-16 09:30:32 -0600

    test bench hazards fixed

--- src/thunderbird_fsm_tb.vhd
+++ src/thunderbird_fsm_tb.vhd
@@ -148,15 +148,20 @@ begin
 		
 		---- both flashing / hazards on 
 		w_right <= '1'; w_left <= '1'; wait for k_clk_period;
-          assert w_leftlights = "111" report "both should be on during hazards" severity failure;
-          assert w_rightlights = "111" report "both should be on during hazards" severity failure; 
-	    wait for k_clk_period; -- lights should turn off 
+  --        assert w_leftlights = "111" report "both should be on during hazards" severity failure;
+  --        assert w_rightlights = "111" report "both should be on during hazards" severity failure; 
+	--    wait for k_clk_period; -- lights should turn off 
           assert w_leftlights = "000" report "both should be off during hazards" severity failure;
           assert w_rightlights = "000" report "both should be off during hazards" severity failure; 
         wait for k_clk_period;
           w_right <= '1'; w_left <= '1'; wait for k_clk_period;
           assert w_leftlights = "111" report "both should be on during hazards" severity failure;
           assert w_rightlights = "111" report "both should be on during hazards" severity failure; 
+        wait for k_clk_period;
+          w_right <= '1'; w_left <= '1'; wait for k_clk_period;
+          assert w_leftlights = "111" report "both should be on during hazards" severity failure;
+          assert w_rightlights = "111" report "both should be on during hazards" severity failure; 
+		
 		wait; 
 		
 	end process;
